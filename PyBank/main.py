d2f8063d48c562a3bbe0e3a1fc5ecc564c429bd5
Bryan <30875957+RoarkJ@users.noreply.github.com>
2020-07-07 15:36:06 -0600

    Final scripts with notes added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,31 @@
 # main.py
 # Resources/budget_data.csv analysis
 
+# Import Dependencies
 import os
 import csv
 
+# Create variable holding path to source csv file
 source_path = os.path.join("Resources", "budget_data.csv")
+# Create varaible holding path to create results text document
 output_file = os.path.join("budget_data_analysis.txt")
 
+# open source file using the with statement to handle cleanup
 with open (source_path, "r") as csvfile:
+	# Create csv.reader object and assign it a variable.
 	csv_reader = csv.reader(csvfile, delimiter = ',')
-	for record in csv_reader:
-		row_data = [{record[0]: int(record[1])} for record in csv_reader]
-		diff = [list(row_data[i + 1].values())[0] - list(row_data[i].values())[0] for i in range(0, len(row_data) - 1)]
-		
+	# Bypass the header data
+	next(csv_reader)
+	# Using a list comprehension go through each record creating a list of dictionaries
+	# holding the key value pairs for date=key and profit/loss=value
+	row_data = [{record[0]: int(record[1])} for record in csv_reader]
+	# Create another list comprehension to determine the difference in 
+	# profit loss from month to month and store resulting data in a list
+	# and associate the list with a variable.
+	diff = [list(row_data[i + 1].values())[0] - list(row_data[i].values())[0] for i in range(0, len(row_data) - 1)]
+
+# Create desired results output text structure with the results 
+# inserted into desired locations within the results text structure
 output = f'''Financial Analysis
 ----------------------------
 Total Months: {len(row_data)}
@@ -21,9 +34,11 @@ Average Change: ${round(sum(diff)/len(diff), 2)}
 Greatest Increase in Profits: {list(row_data[diff.index(max(diff))+ 1].keys())[0]} (${max(diff)})
 Greatest Decrease in Profits: {iter(row_data[diff.index(min(diff))+ 1].keys()).__next__()} (${min(diff)})
 '''
-
+# Print the budget analysis results to terminal for perusal
 print(output)
 
+# Produce the output file and place in the appropriate directory
+# Use with statement to do cleanup after action completes
 with open (output_file, "w") as handle:
 	handle.write(output)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,59 +1,80 @@
 # main.py
 # Resources/election_data.csv tabulation
 
+# Import Dependencies
 import os
 import csv
+import sys
 from operator import itemgetter
 
+# Create variable holding path to source csv file
 source_path = os.path.join("Resources", "election_data.csv")
+# Create varaible holding path to create results text document
 output_file = os.path.join("election_results.txt")
+# Provide container structure containing candidate names and coresponding vote count
+candidates = {"Correy": 0, "Khan": 0, "Li": 0, "O'Tooley": 0}
 
-def vote_count(source):
-	candidates = {"Correy": 0,
-				"Khan": 0,
-				"Li": 0,
-				"O'Tooley": 0
-				}
-
+# Implement function with parameters to recieve needed arguments
+# Design the function to handle any number of candidates
+def vote_count(source, candidates, output):
+	# open source file using the with statement to handle cleanup
 	with open(source, "r") as election_data:
+		# Create csv.reader object and associate to a variable.
 		votes = csv.reader(election_data, delimiter = ",")
+		# Bypass the header data
 		next(votes)
+		
+		# create loop to go through each row of data
 		for vote in votes:
-			if vote[2] == "Khan":
-				candidates["Khan"] += 1
-			elif vote[2] == "Correy":
-				candidates["Correy"] += 1
-			elif vote[2] == "Li":
-				candidates["Li"] += 1
-			elif vote[2] == "O'Tooley":
-				candidates["O'Tooley"] += 1
+			candidate=vote[2]
+			# Implement try|except block to handle possible candidate error in source data
+			try:
+				# read each line of data and asign vote to appropriate candidate
+				candidates[candidate]+=1
+				# If there is a candidate mismatch hadle the error gracefully
+				# catch error and send message to 
+				# notifying election official of error and stop the vote count
+			except:
+				sys.exit('Probable Candidate Reference Error!')
 				
-		total_votes = sum(votes for votes in candidates.values())
+		# Calculate grand total votes from candidate totals
+		total_votes = sum(tally for tally in candidates.values())
+		# Sort the candidates based on votes received by each candidate
+		# Order greatest to least candidate vote count
 		# dict.items() returns a list of k, v, tuple pairs
-		res = sorted(candidates.items(), key=itemgetter(1), reverse=True)
-			
+		res_ord = sorted(candidates.items(), key=itemgetter(1), reverse=True)
+		# Create a list to hold f-string elements that describe the results for each candidate
+		res_output=[]
+		
+		# Implement for loop to produce the f-string for each candidate
+		# and append the resut to the res_output list 
+		for i in range(0, len(res_ord)):
+			res_output.append(f'{res_ord[i][0]}: {res_ord[i][1]/total_votes:.3%} ({res_ord[i][1]})')
+		# Join the individual candidate results f-string into 
+		# one long string with new line formatting creating
+		# desired results output text
+		# join() method takes an iterable and joins it's elements
+		# into one string preserving element format.
+		results_j = '\n'.join(res_output)
+		
+		# Create desired results output text structure with the results-join
+		# inserted into desired location within the results text structure
 		results = f'''Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------
-{res[0][0]}: {res[0][1]/total_votes:.3%} ({res[0][1]})
-{res[1][0]}: {res[1][1]/total_votes:.3%} ({res[1][1]})
-{res[2][0]}: {res[2][1]/total_votes:.3%} ({res[2][1]})
-{res[3][0]}: {res[3][1]/total_votes:.3%} ({res[3][1]})
+{results_j}
 -------------------------
 Winner: {max(candidates, key=candidates.get)}
 -------------------------
 '''	
+	# Produce the output file and place in the appropriate directory
+	# Use with statement to do cleanup after action completes
 	with open(output_file, "w") as handle:
 		handle.write(results)
-		
+	# Print the election results to terminal for perusal
 	print(results)
+# Call the function I created above with the appropriate arguments
+vote_count(source_path, candidates, output_file)
 				
-vote_count(source_path)
-				
-
-							
-
-		
-		
 
